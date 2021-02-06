c39410661dda45288f4c34f52da2c6b2e25caeab
a-fandy <andreasfnd@gmail.com>
2021-02-06 16:20:42 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/login', function () {
+    return view('auth/login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
