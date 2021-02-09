78458f9c36cde048213bd40b22f3be2447b43c78
paul-lestyo <lestyo24@gmail.com>
2021-02-09 09:58:30 +0700

    cek sudah login belum

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
 
     public function index(Request $request)
     {
