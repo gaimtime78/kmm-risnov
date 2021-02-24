8e6ca4da9595f55ef1d29d1b507749ec416abcdd
astikaist <astikaistiningrum@student.uns.ac.id>
2021-02-24 21:31:12 +0700

    tambah fungsi index di controllers direktorat, informasi, pui

--- app/Http/Controllers/User/DirektoratInovasiController.php
+++ app/Http/Controllers/User/DirektoratInovasiController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class DirektoratInovasiController extends Controller
 {
-    //
+    public function index()
+    {
+        return view ('user.direktorat');
+    }
 }
--- app/Http/Controllers/User/InformasiController.php
+++ app/Http/Controllers/User/InformasiController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class InformasiController extends Controller
 {
-    //
+    public function index()
+    {
+        return view ('user.informasi');
+    }
 }
