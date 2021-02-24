dbaf1beaf9c4c3156bdd177cc4e2f4521b15a232
Tajudda <tajudda@gmail.com>
2021-02-24 15:29:15 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/login');
+    return redirect('/home');
 });
 
 Route::get('/home', [App\Http\Controllers\User\HomeController::class, 'index'])->name('home');
