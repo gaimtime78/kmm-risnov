634f48196e95f3ff566f0798c4f0ee87e7b06762
haiiihen <hennynurcahyaningtyas@gmail.com>
2021-02-24 12:07:20 +0700

    view sambutan, visimisi, lppm, navbar, home

---
+++ resources/views/user/tentang/visiMisi.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.user')
+
+@section('css')
+
+@endsection
+
+@section('content')
+<section class="section db p120">
+    <div class="container">
+        <div class="row">
+        </div><!-- end row -->
+    </div><!-- end container -->
+</section><!-- end section -->
+<section class="section gb nopadtop">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+            <p style="text-align:justify">Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Praesent sed nisi eleifend, fermentum orci amet, iaculis libero.Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Praesent sed nisi eleifend, fermentum orci amet, iaculis libero.Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectusNam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Praesent sed nisi eleifend, fermentum orci amet, iaculis libero.Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus. Praesent sed nisi eleifend, fermentum orci amet, iaculis libero.Nam dictum sem, ipsum aliquam . Etiam sit amet fringilla lacus. Pellentesque suscipit ante at ullamcorper pulvinar neque porttitor. Integer lectus</p>
+            </div>
+        </div>
+        
+    <hr class="invis">
+    </div><!-- end container -->
+</section>
+@endsection
+
+@section('js')
+
+@endsection
