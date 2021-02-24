fb3ff4aad5f2ae4425d04f2db0cfa63d8d37d6ed
d3vk <developerkecil@gmail.com>
2021-02-24 09:46:01 +0700

    Menambahkan TinyMCE init sebagai modul js

--- resources/views/admin/page/create.blade.php
+++ resources/views/admin/page/create.blade.php
@@ -113,42 +113,6 @@
 @endsection
 
 @section('js')
-    <script>
-        tinymce.init({
-            selector: 'textarea',
-            plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-            toolbar: 'insert a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table imageupload',
-            toolbar_mode: 'floating',
-            tinycomments_mode: 'embedded',
-            tinycomments_author: 'Author name',
-            file_picker_types: 'file image media',
-            height: 480,
-            block_unsupported_drop: false,
-            menubar:true,
-            setup: function(editor) {
-                let uploader = $('<input id="tinymce-uploader" type="file" accept="image/*" style="display:none">');
-                uploader.on("change",function(){
-                    let input = uploader.get(0);
-                    let file = input.files[0];
-                    let fr = new FileReader();
-                    fr.onload = function() {
-                        let img = new Image();
-                        img.src = fr.result;
-                        editor.insertContent(`<img src="${img.src}"/>`);
-                        uploader.val('');
-                    }
-                    fr.readAsDataURL(file);
-                });
-                editor.ui.registry.addButton('insert', {
-                    icon: 'image',
-                    tooltip: 'Insert',
-                    onAction: function (e){
-                        uploader.trigger('click');
-                    }
-                });
-            }
-        });
-
-    </script>
-    <script type="text/javascript" src="{{ asset('js\plugins\prism\prism.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js\tinyinit.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js\plugins\prism\prism.js') }}"></script>
 @endsection
