013ae2a8ebf330f5ac43760eb4afe3c94bcc28a6
paul-lestyo <lestyo24@gmail.com>
2021-03-02 10:29:25 +0700

    menambah validasi di view menu dan merapikan route

--- routes/web.php
+++ routes/web.php
@@ -53,13 +53,13 @@ Route::middleware(['auth:sanctum','RoleAuth'])->group(function () {
             return view('dashboard');
         })->name('dashboard');
         
-        Route::group(['prefix' => '/menu'], function(){
-            Route::get('/', [App\Http\Controllers\Menu\MenuController::class, 'index'])->name('menu.index');
-            Route::get('/create', [App\Http\Controllers\Menu\MenuController::class, 'add'])->name('menu.add');
-            Route::post('/create', [App\Http\Controllers\Menu\MenuController::class, 'create'])->name('menu.create');
-            Route::get('/edit/{id}', [App\Http\Controllers\Menu\MenuController::class, 'edit'])->name('menu.edit');
-            Route::post('/edit/{id}', [App\Http\Controllers\Menu\MenuController::class, 'update'])->name('menu.update');
-            Route::get('/delete/{id}', [App\Http\Controllers\Menu\MenuController::class, 'delete'])->name('menu.delete');
+        Route::group(['as'=> 'menu.', 'prefix' => '/menu'], function(){
+            Route::get('/', [App\Http\Controllers\Menu\MenuController::class, 'index'])->name('index');
+            Route::get('/create', [App\Http\Controllers\Menu\MenuController::class, 'add'])->name('add');
+            Route::post('/create', [App\Http\Controllers\Menu\MenuController::class, 'create'])->name('create');
+            Route::get('/edit/{id}', [App\Http\Controllers\Menu\MenuController::class, 'edit'])->name('edit');
+            Route::post('/edit/{id}', [App\Http\Controllers\Menu\MenuController::class, 'update'])->name('update');
+            Route::get('/delete/{id}', [App\Http\Controllers\Menu\MenuController::class, 'delete'])->name('delete');
         });
         
         Route::group(['prefix' => '/category'], function(){
@@ -80,13 +80,6 @@ Route::middleware(['auth:sanctum','RoleAuth'])->group(function () {
             Route::put('/admin/post/{id}', [PostController::class, 'update'])->name('post.update');
             Route::delete('/admin/post/{id}', [PostController::class, 'delete'])->name('post.delete');
         });
-
-        // Route::group(['prefix' => '/posts'], function(){
-        //     // Route::get('/', 'historyController@index')->name('history');
-        //     // Route::get('/{id}', 'historyController@show');
-        //     Route::get('/create', [App\Http\Controllers\Posts\PostsController::class, 'add'])->name('posts.add');
-        //     Route::post('/create', [App\Http\Controllers\Posts\PostsController::class, 'create'])->name('posts.create');
-        // });
 
         Route::get('/post', [PostController::class, 'index'])->name('post.index');
         Route::get('/post/category/{id}', [PostController::class, 'category'])->name('post.category');
