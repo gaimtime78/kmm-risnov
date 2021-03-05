801cee4ff05cf5df4281b6fe84c5cdb990b016a9
d3vk <developerkecil@gmail.com>
2021-03-05 10:40:14 +0700

    Sync footer dengan uns.ac.id

--- resources/views/layout/navigation/footer.blade.php
+++ resources/views/layout/navigation/footer.blade.php
@@ -2,63 +2,114 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4 col-md-4">
-                        <div class="widget clearfix">
-                            <h3 class="widget-title">Subscribe Our Newsletter</h3>
-                            <div class="newsletter-widget">
-                                <p>You can opt out of our newsletters at any time.<br> See our <a href="#">privacy policy</a>.</p>
-                                <form class="form-inline" role="search">
-                                    <div class="form-1">
-                                        <input type="text" class="form-control" placeholder="Enter email here..">
-                                        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane-o"></i></button>
-                                    </div>
-                                </form>
-                                <img src="images/payments.png" alt="" class="img-responsive">
-                            </div><!-- end newsletter -->
-                        </div><!-- end widget -->
+                        <h4 class="widget-title">Layanan Sistem</h4>
+                        <div class="layanan-sistem">
+                            <ul>
+                                <li><a target="_blank" rel="noopener" href="https://ocw.uns.ac.id">Open Course Ware
+                                        UNS</a></li>
+                                <li><a target="_blank" href="https://spada.uns.ac.id">SPADA UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://siakad.uns.ac.id">SIAKAD UNS</a>
+                                </li>
+                                <li><a target="_blank" rel="noopener" href="https://simpeg.uns.ac.id">SIMPEG UNS</a>
+                                </li>
+                                <li><a target="_blank" rel="noopener" href="https://sipsmart.uns.ac.id">Prestasi
+                                        Mahasiswa UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://eprints.uns.ac.id/">Institutional
+                                        Repository UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://digilib.uns.ac.id">Digital Library
+                                        UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://jurnal.uns.ac.id">E-Journal UNS</a>
+                                </li>
+                                <li><a target="_blank" rel="noopener" href="https://profil.uns.ac.id/">Email UNS</a>
+                                </li>
+                                <li><a target="_blank" rel="noopener" href="https://jadwal.uns.ac.id/">Generate Jadwal
+                                        UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://iris1103.uns.ac.id/">Riset dan
+                                        Inovasi UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://civitas.uns.ac.id">Blog UNS</a></li>
+                                <li><a target="_blank" rel="noopener"
+                                        href="https://remunerasi.uns.ac.id/web/">Remunerasi UNS</a></li>
+                                <li><a target="_blank" rel="noopener" href="https://perencanaan.uns.ac.id/">Perencanaan
+                                        Terpadu UNS</a></li>
+                                <li><a target="_blank" href="https://sikd.uns.ac.id">Kearsipan Dinamis UNS</a></li>
+                                <li><a target="_blank" href="https://sibea.mawa.uns.ac.id/">Pelayanan Beasiswa UNS</a>
+                                </li>
+                                <li><a target="_blank" href="https://sia.uns.ac.id/pajak/">SIA Pendapatan &#038; Pajak
+                                        UNS</a></li>
+                                <li><a target="_blank" href="https://sia.uns.ac.id/sipp/">SIA SIPP UNS</a></li>
+                                <li><a target="_blank" href="https://sister.uns.ac.id/">Sumberdaya Terintegrasi UNS</a>
+                                </li>
+                                <li><a target="_blank" href="https://b2b.uns.ac.id/">Dokumentasi Kerja Sama UNS</a></li>
+                            </ul>
+                        </div>
                     </div><!-- end col -->
 
                     <div class="col-lg-3 col-md-3">
                         <div class="widget clearfix">
-                            <h3 class="widget-title">Join us today</h3>
-                            <p>Would you like to earn your profits by joining our team? Join us without losing time.</p>
-                            <a href="#" class="readmore">Became a Teacher</a>
-                        </div><!-- end widget -->
-                    </div><!-- end col -->
-
-                    <div class="col-lg-3 col-md-3">
-                        <div class="widget clearfix">
-                            <h3 class="widget-title">Popular Tags</h3>
-                            <div class="tags-widget">   
-                                <ul class="list-inline">
-                                    <li><a href="#">course</a></li>
-                                    <li><a href="#">web design</a></li>
-                                    <li><a href="#">development</a></li>
-                                    <li><a href="#">language</a></li>
-                                    <li><a href="#">teacher</a></li>
-                                    <li><a href="#">speaking</a></li>
-                                    <li><a href="#">material</a></li>
-                                    <li><a href="#">css3</a></li>
-                                    <li><a href="#">html</a></li>
-                                    <li><a href="#">learning</a></li>
-                                </ul>
-                            </div><!-- end list-widget -->
-                        </div><!-- end widget -->
-                    </div><!-- end col -->
-
-                    <div class="col-lg-2 col-md-2">
-                        <div class="widget clearfix">
-                            <h3 class="widget-title">Support</h3>
-                            <div class="list-widget">   
+                            <h4 class="widget-title">Portal Informasi</h4>
+                            <div class="portal-informasi">
                                 <ul>
-                                    <li><a href="#">Terms of Use</a></li>
-                                    <li><a href="#">Copyrights</a></li>
-                                    <li><a href="#">Create a Ticket</a></li>
-                                    <li><a href="#">Pricing & Plans</a></li>
-                                    <li><a href="#">Carrier</a></li>
-                                    <li><a href="#">Trademark</a></li>
+                                    <li><a href="https://uns.ac.id">Laman UNS</a></li>
+                                    <li>
+                                        <a target="_blank" rel="noopener" href="https://spmb.uns.ac.id">Penerimaan
+                                            Mahasiswa
+                                            Baru UNS</a>
+                                    </li>
+                                    <li><a href="https://akademik.uns.ac.id">Akademik UNS</a></li>
+                                    <li><a href="https://kepeg.auk.uns.ac.id/">Kepegawaian UNS</a></li>
+                                    <li><a href="https://mawa.uns.ac.id/">Kemahasiswaan UNS</a></li>
+                                    <li>
+                                        <a target="_blank" rel="noopener" href="https://koran.uns.ac.id">UNS on News</a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener" href="https://newsroom.uns.ac.id">UNS
+                                            Event</a>
+                                    </li>
+                                    <li><a target="_blank" rel="noopener" href="https://photostock.uns.ac.id/">Galeri
+                                            UNS</a></li>
+                                    <li><a target="_blank" rel="noopener" href="https://rs.uns.ac.id">Rumah Sakit
+                                            UNS</a>
+                                    </li>
+                                    <li>
+                                        <a href="https://uns.ac.id/id/ukm/">Kiprah Mahasiswa UNS</a>
+                                    </li>
+                                    <li><a target="_blank" rel="noopener" href="https://karyaku.uns.ac.id">Karya Ilmiah
+                                            Pendidik UNS</a></li>
+                                    <li><a href="https://greencampus.uns.ac.id">UNS Green Campus</a></li>
+                                    <li>
+                                        <a target="_blank" rel="noopener"
+                                            href="https://uns.ac.id/id/category/uns-research/">Produk &#038;
+                                            Penelitian</a>
+                                    </li>
+                                    <li>
+                                        <a target="_blank" rel="noopener"
+                                            href="https://uns.ac.id/id/category/uns-berkarya/">Sivitas UNS
+                                            Berkarya</a>
+                                    </li>
+                                    <li><a href="https://rb.uns.ac.id/">Reformasi Birokrasi UNS</a></li>
+                                    <li><a target="_blank" rel="noopener" href="https://video.uns.ac.id">Video UNS</a>
+                                    </li>
+                                    <li><a href="https://uns.ac.id/id/pidato/">Sambutan Rektor UNS</a></li>
+                                    <li><a href="https://alumni.uns.ac.id">Alumni UNS</a></li>
+                                    <li><a href="https://jawametrik.uns.ac.id/">Kearifan Lokal UNS</a></li>
+                                    <li>
+                                        <a target="_blank" rel="noopener" href="https://cdc.uns.ac.id">Lowongan Kerja
+                                            dan
+                                            Karir</a>
+                                    </li>
                                 </ul>
-                            </div><!-- end list-widget -->
+                            </div>
                         </div><!-- end widget -->
+                    </div><!-- end col -->
+
+                    <div class="col-lg-3 col-md-3">
+                        <h4 class="widget-title">Lokasi</h4>
+                        <div class="textwidget">
+                            <p><iframe style="width:100%; height: 480px;"
+                                    src="https://widgets.moovit.com/w/66345083B5CBDD8FE0530100007FE639/874784"
+                                    frameborder="0"></iframe>
+                            </p>
+                        </div>
                     </div><!-- end col -->
                 </div><!-- end row -->
             </div><!-- end container -->
@@ -77,7 +128,8 @@
                         <div class="footer-links">
                             <ul class="list-inline">
                                 <li>Design : <a href="https://html.design">HTML.Design</a></li>
-                                <li>Distributed by : <a href="https://themewagon.com/" target="_blank">ThemeWagon</a></li>
+                                <li>Distributed by : <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+                                </li>
                             </ul>
                         </div>
                     </div>
