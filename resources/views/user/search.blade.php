e2d92e199992184b39508e2f7cc3c57d69a84f6d
NYA2107 <imamnajibulloh.in@gmail.com>
2021-05-20 21:33:35 +0700

    update header search post

--- resources/views/user/search.blade.php
+++ resources/views/user/search.blade.php
@@ -33,7 +33,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="tagline-message page-title text-center">
-                            <h3>POST</h3>
+                            <h3>{{$category}}</h3>
                         </div>
                     </div><!-- end col -->
                 </div><!-- end row -->
