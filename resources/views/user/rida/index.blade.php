d289e46a1d11be58f9bd6a9a4b88140312a8713a
DESKTOP-NBHVLPE\CORP KAPID <yoga354@hotmail.com>
2021-08-30 15:02:55 +0700

    rename button user/rida/indeks

--- resources/views/user/rida/index.blade.php
+++ resources/views/user/rida/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="tagline-message page-title text-center">
-                        <h3>Dokumentasi RIDA</h3>
+                        <h3 style="color: #000000;">Dokumentasi RIDA</h3>
                         </div>
                     </div><!-- end col -->
                 </div><!-- end row -->
@@ -17,49 +17,56 @@
         </section><!-- end section -->
         <section class="section gb nopadtop">
           <div class="container">
-              <div class="row">
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Doktoral</h4>
-                          <a href="{{route('rida-doktoral')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Magister</h4>
-                          <a href="{{route('rida-magister')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Spesialis-2</h4>
-                          <a href="{{route('rida-spesialis-2')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Spesialis Konsultan</h4>
-                          <a href="{{route('rida-spesialis-konsultan')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Spesialis-1</h4>
-                          <a href="{{route('rida-spesialis-1')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
-                  <div class="col-md-3">
-                      <div class="box m30">
-                          <i class="flaticon-monitor-tablet-and-smartohone"></i>
-                          <h4>Profesi</h4>
-                          <a href="{{route('rida-profesi')}}" class="readmore">Pelajari Selengkapnya</a>
-                      </div>
-                  </div><!-- end col -->
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG DOKTOR</h4>
+                            <a href="{{route('rida-doktoral')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG MAGISTER</h4>
+                            <a href="{{route('rida-magister')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG SPESIALIS-2</h4>
+                            <a href="{{route('rida-spesialis-2')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
+                </div>
+                
+                <hr class="invis">
+                <hr class="invis">
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG SPESIALIS-1 (K)</h4>
+                            <a href="{{route('rida-spesialis-konsultan')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
+               
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG SPESIALIS-1</h4>
+                            <a href="{{route('rida-spesialis-1')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
+                    <div class="col-md-4">
+                        <div class="box m30">
+                            <i class="flaticon-monitor-tablet-and-smartohone"></i>
+                            <h4>RENTANG USIA PRODUKTIF PENELITI DAN PENGABDI TENAGA PENDIDIK JENJANG PROFESI</h4>
+                            <a href="{{route('rida-profesi')}}" class="readmore">Selengkapnya</a>
+                        </div>
+                    </div><!-- end col -->
                   
               </div><!-- end row -->
               <div class="row">
